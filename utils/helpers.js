850313484e95877579fa5fa51cf02acb97f76cc2
Tyler Rudman <tyler.rudman@gmail.com>
2023-02-08 22:06:19 -0500

    added date helper using date-fns

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,19 @@
+// module.exports = {
+//   format_date: (date) => {
+//     const dateObj = new Date();
+//     return `${dateObj.getUTCMonth() + 1}/${dateObj.getUTCDate() - 1}/${dateObj.getUTCFullYear()}`;
+
+//     // return `${new Date(date).getDate()}/${new Date(date).getMonth() + 1}/${new Date(
+//     //   date
+//     // ).getFullYear()}`;
+//   },
+// };
+
+const { format } = require("date-fns");
+
 module.exports = {
   format_date: (date) => {
     const dateObj = new Date();
-    return `${dateObj.getUTCMonth() + 1}/${dateObj.getUTCDate() - 1}/${dateObj.getUTCFullYear()}`;
-
-    // return `${new Date(date).getDate()}/${new Date(date).getMonth() + 1}/${new Date(
-    //   date
-    // ).getFullYear()}`;
+    return `${format(dateObj, "MM-dd-yyyy")}`;
   },
 };
