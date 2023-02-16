6dc3ba9d3e6afb6624e34f7cac836f534e2dd25e
Jakob A <jdanders1999@gmail.com>
2023-02-15 20:10:49 -0500

    Took out unnecessary code and added comments, fixed capitalization

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,3 @@
-// module.exports = {
-//   format_date: (date) => {
-//     const dateObj = new Date();
-//     return `${dateObj.getUTCMonth() + 1}/${dateObj.getUTCDate() - 1}/${dateObj.getUTCFullYear()}`;
-
-//     // return `${new Date(date).getDate()}/${new Date(date).getMonth() + 1}/${new Date(
-//     //   date
-//     // ).getFullYear()}`;
-//   },
-// };
-
 const { format } = require("date-fns");
 
 module.exports = {
